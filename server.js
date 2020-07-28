4589428f61c940f7704aac78f99921e16f1b2448
suhai <suhai.yehuza@cloudera.com>
2020-07-28 10:56:54 -0700

    Merge test app with main app

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use('/api/messages', require('./routes/api/messages'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
